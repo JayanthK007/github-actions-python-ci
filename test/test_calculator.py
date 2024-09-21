ec136c258ea2e779c4865f14d27479ce22019e39
Jayanth <jayanthfaraday@gmail.com>
2024-09-20 17:10:07 -0700

    second  commit

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -11,12 +11,14 @@ class TestCalculator(unittest.TestCase):
 def test_subtract(self):
   self.assertEqual(subtract(10, 3), 7)
   self.assertEqual(subtract(-3, 1), -4)
+  self.assertEqual(subtract(-3, -3), 0)
 
 
 def test_divide(self):
   self.assertEqual(divide(4,2),2)
-  self.assertEqual(divide(-4,2),-2)
+  self.assertEqual(divide(6,2),3)
+  self.assertEqual(divide(20,10),2)
 
-  
+
 if __name__ == '__main__':
    unittest.main()
